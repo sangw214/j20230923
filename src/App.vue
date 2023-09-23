8fa11cb57168743af8074233e136889f48cea6b9
sangw214 <hybridspirit@gmail.com>
2023-09-23 10:17:31 +0900

    create 20230923

---
+++ src/App.vue
@@ -0,0 +1,210 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar
+      color="surface-variant"
+      title="영업사원 방문관리" />
+
+    <v-main
+      class="flex-nowrap align-center justify-center"
+      style="min-width: 640px;min-height: 250px;">
+      <v-container>
+        <v-row
+          justify="center"
+          class="bg-white my-1"
+          no-gutters>
+          <v-col cols="7">
+            <v-card>
+              <v-date-picker
+                no-title
+                v-model="picker" />
+            </v-card>
+          </v-col>
+          <v-col cols="3">
+            <v-sheet class="pa-2 ma-2">
+              <v-card>
+                오늘
+                <v-card height="150">
+                  4건
+                </v-card>
+                완료
+                <v-card height="150">
+                  15건
+                </v-card>   
+              </v-card>
+            </v-sheet>
+          </v-col>
+          <v-col cols="10">
+            <v-sheet class="pa-2 ma-2">
+              방문등록
+              <v-card style="overflow-x:auto; white-space: nowrap;">
+                <v-data-table
+                  v-model="selected"
+                  v-model:items-per-page="itemsPerPage"
+                  :headers="headers"
+                  :items="desserts"
+                  item-value="name"
+                  show-select
+                  class="elevation-1" />              
+              </v-card>
+            </v-sheet>
+            <v-row justify="center">
+              <v-col
+                cols="auto"
+                sm="3">
+                <v-btn
+                  block
+                  rounded="lg"
+                  size="large">
+                  등록
+                </v-btn>
+              </v-col>
+              <v-col
+                cols="auto"
+                sm="3">
+                <v-btn
+                  block
+                  rounded="lg"
+                  size="large">
+                  삭제
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-col>          
+        </v-row>
+      </v-container>
+    </v-main>
+    
+    <v-footer
+      app
+      name="footer"
+      class="d-flex align-center justify-center"
+      style="min-height: 10px;">
+      footer Content
+    </v-footer>    
+  </v-layout>
+</template>
+
+<script>
+import { VDatePicker } from 'vuetify/labs/VDatePicker'
+import { VDataTable } from "vuetify/labs/VDataTable";
+
+export default ({
+  components: {
+    VDatePicker,
+    VDataTable,
+  },
+  data(){
+    return {
+      picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      itemsPerPage: 5,
+      selected: [],
+      headers: [
+          {
+            title: '시간',
+            align: 'start',
+            sortable: false,
+            key: 'name',
+          },
+          { title: '방문시간', align: 'end', key: 'calories' },
+          { title: '방문자', align: 'end', key: 'fat' },
+          { title: '주소', align: 'end', key: 'carbs' },
+          { title: '연락처', align: 'end', key: 'protein' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+          },
+        ],      
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.v-container {
+  padding: 5;
+}
+div{
+  .v-sheet{
+    .v-picker-title, 
+    .v-date-picker-header, 
+    .v-picker__actions {
+      display: none;
+    }  
+  }
+}
+div{
+  .v-data-table-footer__items-per-page,
+  .v-data-table-footer__info{
+    display: none;
+  }
+
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-sheet>
+    <v-card
+      class="mx-auto pa-12 pb-8"
+      max-width="480"
+      rounded="lg">
+      <v-container fluid>
+        <v-form
+          v-model="form"
+          @submit.prevent="login">
+          <div class="text-subtitle-1 text-medium-emphasis">
+            아이디
+          </div>
+
+          <v-text-field
+            v-model="id"
+            variant="outlined"
+            density="compact"
+            placeholder="아이디 입력" />
+          
+          <div class="text-subtitle-1 text-medium-emphasis">
+            패스워드
+          </div>
+
+          <v-text-field
+            v-model="password"
+            :type="'password'"
+            variant="outlined"
+            density="compact"
+            @click:append-inner="visible = !visible"
+            placeholder="패스워드 입력" />
+
+          <v-checkbox
+            v-model="terms"
+            color="secondary"
+            label="아이디 저장" />
+
+          <br />
+          <br />
+
+          <v-btn
+            block
+            color="success"
+            size="large"
+            type="submit"
+            variant="elevated"
+            @click="login();">
+            로그인
+          </v-btn>
+        </v-form>
+      </v-container>
+    </v-card>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      id: null,
+      password: null,
+      terms: false
+    }
+  },
+  methods:{
+    login(){
+      this.$router.push({ name: 'Schedule' })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.v-sheet {
+    height: 428px;
+    width: 380px;
+    margin: 0 auto;
+    position: absolute;
+    top:50%;
+    transform: translateY(-50%); 
+    left:0;
+    right: 0;
+}
+</style>
